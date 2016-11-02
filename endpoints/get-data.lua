1170415896ac9551ddf5d5ad79d38cb5b6c5d386
Andrew Solz <andrewsolz@exosite.com>
2016-11-02 16:43:56 -0500

    make endpoint work

--- endpoints/get-data.lua
+++ endpoints/get-data.lua
@@ -1,2 +1,2 @@
 --#ENDPOINT get /data
-response.message = Timeseries.query({q='select * from "data" group by sn order by desc limit 500'})
+response.message = Timeseries.query({q='select * from "data" WHERE timestamp > now() - 1d group by sn order by desc limit 500'})
