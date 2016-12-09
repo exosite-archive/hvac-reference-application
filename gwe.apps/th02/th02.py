651db8fde819d047ccdc2af7ad609daed6c89133
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-12-09 12:17:09 -0600

    Starting conversion of th02 into GWE app

--- gwe.apps/th02/th02.py
+++ gwe.apps/th02/th02.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import subprocess
 import time
 
@@ -20,13 +21,13 @@ REG_ADDR_HYST = 0x05
 getData = int()
 analogVal = int()
 
-       
+
 
 class I2C_TH02():
     def __init__(self):
         self.i2c = Adafruit_I2C(ADDR_TH02, BUS_TH02)
         self.cik = subprocess.Popen(["gwe", "-C"], stdout=subprocess.PIPE).communicate()[0].rstrip()
-        
+
     def conv_temp(self):
         self.i2c.write8(REG_ADDR_CONFIG, 0x11)
         time.sleep(0.25)
@@ -39,7 +40,7 @@ class I2C_TH02():
         temp /= 32
         temp -= 50
         return temp
-        
+
     def conv_humidity(self):
         self.i2c.write8(REG_ADDR_CONFIG, 0x01)
         time.sleep(0.25)
@@ -52,23 +53,23 @@ class I2C_TH02():
         humi /= 16
         humi -= 24
         return humi
-        
+
     def conv_ready(self):
         conv_status = self.i2c.read8(REG_ADDR_STATUS)
         if conv_status == 0:
             return True
         else:
             return False
-        
-    def read_adc(self):     
-        data = self.i2c.readS16(REG_ADDR_RESULT)      
+
+    def read_adc(self):
+        data = self.i2c.readS16(REG_ADDR_RESULT)
         return data
-        
+
     def upload_data(self, temp, humi):
         data = {'temperature': temp, 'humidity': humi}
         headers = {'X-Exosite-CIK': self.cik}
         exo_req = requests.post('https://m2.exosite.com/onep:v1/stack/alias?state', data=data, headers=headers)
-        
+
 if __name__=="__main__":
     th02 = I2C_TH02()
     while True:
@@ -78,4 +79,4 @@ if __name__=="__main__":
         print("{}%".format(humi))
         exo_req = th02.upload_data(temp, humi)
         time.sleep(5)
-        
+
