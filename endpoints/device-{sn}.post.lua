0439050df3ff5da91e203c04a86a1376d595ee78
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-12-09 11:01:18 -0600

    Also write to tsdb for device updates.
    AD-37

--- endpoints/device-{sn}.post.lua
+++ endpoints/device-{sn}.post.lua
@@ -8,13 +8,28 @@ if sn == nil then
 end
 local pid = Config.solution().products[1]
 
-local r = Device.write({
+local ret = {}
+
+local dr = Device.write{
 	pid=pid,
 	device_sn=sn,
 	desired_temperature=request.body.desired_temperature,
 	heat_on=request.body.heat_on,
 	ac_on=request.body.ac_on,
-})
+}
+ret.device = dr
 
-return r
+local tr = Tsdb.write{
+	tags = {sn=sn},
+	metrics = {
+		desired_temperature=request.body.desired_temperature,
+		heat_on=request.body.heat_on,
+		ac_on=request.body.ac_on,
+	}
+}
+if tr.error ~= nil then
+	ret.tsdb = tr
+end
+
+return ret
 -- vim: set ai sw=2 ts=2 :
