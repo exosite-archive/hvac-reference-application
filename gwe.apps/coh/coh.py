707218d2fe0b25ebc3dce7aee54fab7721c80465
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-12-12 11:05:19 -0600

    remove extra comments

--- gwe.apps/coh/coh.py
+++ gwe.apps/coh/coh.py
@@ -15,16 +15,9 @@ class CoolOrHeat():
 
   def waitForData(self):
     self.api._http_long_poll('change', 300000)
-    #headers = {
-    #    'X-Exosite-CIK': self.cik,
-    #    'Request-Timeout': 30000
-    #    }
-    #requests.post('https://m2.exosite.com/onep:v1/stack/alias?change', headers=headers)
 
   def readData(self):
     self.api.http_read(['ac_on','heat_on'])
-    #headers = {'X-Exosite-CIK': self.cik}
-    #requests.post('https://m2.exosite.com/onep:v1/stack/alias?ac_on&heat_on', headers=headers)
 
 if __name__=="__main__":
   ch = CoolOrHeat()
