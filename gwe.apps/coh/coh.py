4c8365c2421ad59f7a4a66ceb3a941610c384bba
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-12-12 11:04:51 -0600

    python is indention errors.

--- gwe.apps/coh/coh.py
+++ gwe.apps/coh/coh.py
@@ -13,18 +13,18 @@ class CoolOrHeat():
     self.cik = subprocess.Popen(["gwe", "-C"], stdout=subprocess.PIPE).communicate()[0].rstrip()
     self.api = ExositeAPI(cik=self.cik)
 
-    def waitForData(self):
-      self.api._http_long_poll('change', 300000)
-      #headers = {
-      #    'X-Exosite-CIK': self.cik,
-      #    'Request-Timeout': 30000
-      #    }
-      #requests.post('https://m2.exosite.com/onep:v1/stack/alias?change', headers=headers)
+  def waitForData(self):
+    self.api._http_long_poll('change', 300000)
+    #headers = {
+    #    'X-Exosite-CIK': self.cik,
+    #    'Request-Timeout': 30000
+    #    }
+    #requests.post('https://m2.exosite.com/onep:v1/stack/alias?change', headers=headers)
 
-    def readData(self):
-      self.api.http_read(['ac_on','heat_on'])
-      #headers = {'X-Exosite-CIK': self.cik}
-      #requests.post('https://m2.exosite.com/onep:v1/stack/alias?ac_on&heat_on', headers=headers)
+  def readData(self):
+    self.api.http_read(['ac_on','heat_on'])
+    #headers = {'X-Exosite-CIK': self.cik}
+    #requests.post('https://m2.exosite.com/onep:v1/stack/alias?ac_on&heat_on', headers=headers)
 
 if __name__=="__main__":
   ch = CoolOrHeat()
