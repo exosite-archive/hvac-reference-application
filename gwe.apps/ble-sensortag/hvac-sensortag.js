d1d361aa27f7fe72d5402529c5c52eb3b07c7289
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-01-20 14:31:25 -0600

    Use async to help get CIK.

--- gwe.apps/ble-sensortag/hvac-sensortag.js
+++ gwe.apps/ble-sensortag/hvac-sensortag.js
@@ -2,45 +2,61 @@
 //
 "use strict";
 var SensorTag = require('sensortag');
-//var async = require('async');
+var exec = require('child_process').exec;
+var async = require('async');
+
+var CIK = null;
+var Tags = [];
 
 console.log("Begins.");
-var Tags = [];
-SensorTag.discoverAll(function(st) {
-	console.log("Discovered: ", st.type, " - ", st.id);
-	st.connectAndSetup(function(err){
-		if (err) {
-			console.log("Failed to connect and setup tag: ", err);
-		} else {
-			console.log("Setup complete for ", st.id);
-			st.on('humidityChange', function(temperature, humidity) {
-				console.log("UPDATE: ", temperature, humidity);
-			});
-			st.enableHumidity(function(err){
+
+async.series([
+	function(callback) {
+		exec("gwe -C", function(err, stderr, stdout){
+			if (err) {
+				return callback(err, null);
+			}
+			CIK = stdout.trim();
+			callback(null, CIK);
+		});
+	},
+	function(callback) {
+		SensorTag.discoverAll(function(st) {
+			console.log("Discovered: ", st.type, " - ", st.id);
+			st.connectAndSetup(function(err){
 				if (err) {
 					console.log("Failed to connect and setup tag: ", err);
 				} else {
-					console.log("Humidity enabled for ", st.id);
-					st.setHumidityPeriod(5000, function(err){
-						if (err) {
-							console.log("Failed to set period: ", err);
-						} else {
-							console.log("Period set");
-						}
-						st.notifyHumidity(function(err){
-							if (err) {
-								console.log("Failed to enable notify: ", err);
-							} else {
-								console.log("Notify enabled");
-							}
-						});
+					console.log("Setup complete for ", st.id);
+					st.on('humidityChange', function(temperature, humidity) {
+						console.log("UPDATE: ", temperature, humidity);
 					});
+					st.enableHumidity(function(err){
+						if (err) {
+							console.log("Failed to connect and setup tag: ", err);
+						} else {
+							console.log("Humidity enabled for ", st.id);
+							st.setHumidityPeriod(5000, function(err){
+								if (err) {
+									console.log("Failed to set period: ", err);
+								} else {
+									console.log("Period set");
+								}
+								st.notifyHumidity(function(err){
+									if (err) {
+										console.log("Failed to enable notify: ", err);
+									} else {
+										console.log("Notify enabled");
+									}
+								});
+							});
+						}
+					});
+					Tags.push(st);
 				}
 			});
-			Tags.push(st);
-		}
-	});
-
-});
+		});
+	}
+]);
 
 //	vim: set ai sw=2 ts=2 :
