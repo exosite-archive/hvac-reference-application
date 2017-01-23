fb34abfb2c83309832f104ecbe64dae5eae53602
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-01-23 11:49:07 -0600

    update for the smartConnect

--- gwe.apps/ble-smartconnect/hvac-smartconnect.js
+++ gwe.apps/ble-smartconnect/hvac-smartconnect.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
-//
-"use strict";
-var SensorTag = require('sensortag');
+'use strict';
+var SmartConnect = require('./Atmel-SmartConnect');
 var exec = require('child_process').exec;
 var async = require('async');
-var https = require("follow-redirects").https;
-var urllib = require("url");
-var querystring = require("querystring");
+var https = require('follow-redirects').https;
+var urllib = require('url');
+var querystring = require('querystring');
 
 var CIK = null;
 var Tags = [];
-var productID = "";
+var productID = '';
 
 function sendUp(temperature, humidity) {
 	var opts = urllib.parse('https://'+productID+'m2.exosite.com/onep:v1/stack/alias');
@@ -22,64 +21,62 @@ function sendUp(temperature, humidity) {
 	var payload = querystring.stringify({temperature: temperature, humidity: humidity});
 	opts.headers['content-length'] = Buffer.byteLength(payload);
 
-	console.log("Sending:", productID, ">>", payload);
+	console.log('Sending:', productID, '>>', payload);
 
 	var req = https.request(opts, function(result){
-		result.on('data', function (chunk) {});
-		result.on('end',function() {
-		});
+		result.on('data', function () {});
+		result.on('end',function() { });
 	});
 	req.on('error',function(err) {
-		console.log("http write error: ", err);
+		console.log('http write error: ', err);
 	});
 	req.write(payload);
 	req.end();
 }
 
-console.log("Begins.");
+console.log('Begins.');
 
 async.series([
 	function(callback) {
-		exec("gwe -C", function(err, stdout, stderr){
+		exec('gwe -C', function(err, stdout, stderr){
 			if (err) {
 				return callback(err, null);
+			}
+			if (stderr) {
+				console.log('GWE stderr: ', stderr);
 			}
 			CIK = stdout.trim();
 			callback(null, CIK);
 		});
 	},
 	function(callback) {
-		SensorTag.discoverAll(function(st) {
-			console.log("Discovered: ", st.type, " - ", st.id);
+		SmartConnect.discoverAll(function(st) {
+			console.log('Discovered: ', st.type, ' - ', st.id);
 			st.connectAndSetup(function(err){
 				if (err) {
-					console.log("Failed to connect and setup tag: ", err);
+					console.log('Failed to connect and setup tag: ', err);
+					callback(err, null);
 				} else {
-					console.log("Setup complete for ", st.id);
-					st.on('humidityChange', function(temperature, humidity) {
-						//console.log("UPDATE: ", temperature, humidity);
+					console.log('Setup complete for ', st.id);
+					st.on('environmentChange', function(temperature, pressure, uv, humidity) {
+						console.log('UPDATE: ', temperature, pressure, uv, humidity);
 						sendUp(temperature, humidity);
 					});
-					st.enableHumidity(function(err){
+					st.setEnvionmentDataRate(0, function(err){
 						if (err) {
-							console.log("Failed to connect and setup tag: ", err);
+							console.log('Failed to set data rate: ', err);
 						} else {
-							console.log("Humidity enabled for ", st.id);
-							st.setHumidityPeriod(5000, function(err){
-								if (err) {
-									console.log("Failed to set period: ", err);
-								} else {
-									console.log("Period set");
-								}
-								st.notifyHumidity(function(err){
-									if (err) {
-										console.log("Failed to enable notify: ", err);
-									} else {
-										console.log("Notify enabled");
-									}
-								});
-							});
+							console.log('Data rate set');
 						}
+						st.notifyEnvironment(function(err){
+							if (err) {
+								console.log('Failed to enable notify: ', err);
+								callback(err, null);
+							} else {
+								console.log('Notify enabled');
+								callback(null, 'ok');
+							}
+						});
 					});
 					Tags.push(st);
 				}
