fdd5591b0e53b22624abfc9d66a05e9644f59a3b
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-03-30 11:53:49 -0500

    fixed device-sn
    AD-74

--- routes/device-{sn}.get.lua
+++ routes/device-{sn}.get.lua
@@ -17,21 +17,30 @@ local data = Tsdb.query{
 		'humidity',
 		'temperature'
 	},
-  fill = "null",
+	fill = 0,
 	sampling_size = '5m',
 }
 
+if request.parameters.raw ~= nil then return data end
+
 local tsd = util.parse_results{sn=sn, data=data}
+if request.parameters.rawd ~= nil then return tsd end
+local clm_idx = table.find(data.columns, 'desired_temperature')
+if clm_idx > 0 and #data.values > 0 then
+	tsd.desired_temperature = data.values[1][clm_idx]
+end
+--[======[
 local dtemp = Tsdb.query{
-    tags={sn=sn},
-    metrics = {
-        'desired_temperature'
-    },
-    limit = 1
+		tags={sn=sn},
+		metrics = {
+				'desired_temperature'
+		},
+		limit = 1
 }
 if dtemp.values ~= nil then
-  tsd.desired_temperature = dtemp.values[1][2]
+	tsd.desired_temperature = dtemp.values[1][2]
 end
+--]======]
 return tsd
 
 -- vim: set ai sw=2 ts=2 :
