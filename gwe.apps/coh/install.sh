793186264985819e78133ae6b18e44a42da197d2
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-03-31 10:38:17 -0500

    make executable

--- gwe.apps/coh/install.sh
+++ gwe.apps/coh/install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mkdir -p /usr/local/bin
 cp -f coh.py /usr/local/bin/
+chmod +x /usr/local/bin/coh.py
--- gwe.apps/th02/install.sh
+++ gwe.apps/th02/install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mkdir -p /usr/local/bin
 cp -f th02.py /usr/local/bin/
+chmod +x /usr/local/bin/th02.py
